d9100ac8bc3aa96a6826f25864783c7b38ff96d9
PROGRAMACIÓN <PROGRAMACIN@PC-7>
2016-07-27 12:44:04 +0200

    IMC

---
+++ imc.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+
+int main() {
+	float altura;
+	float peso;
+	float resultadoimc;
+	printf("Escribir la altura\n");
+	scanf("%f",&altura);
+	printf("Escribir tu peso\n");
+	scanf("%f",&peso);
+
+	resultadoimc = peso/(altura*altura);
+	printf("Tu IMC es %f\n",resultadoimc);
+	if (resultadoimc<18.5) {
+		printf("Delgadez\n");
+	} else {
+		if (resultadoimc<25) {
+			printf("Peso normal\n");
+		} else {
+			if (resultadoimc<30) {
+				printf("Sobrepeso\n");
+			} else {
+				printf("Obeso\n");
+			}
+		}
+	}
+	return 0;
+}
+
