a7e88835eecf5e775c7055e7168fa5ecdc944310
Bernardo <Programacin@Sistemas35>
2016-09-01 10:30:18 +0200

    Subimos version de Menu Principal como archivo main, rectificamos tambien el borrado del buffer.

--- main.c
+++ main.c
@@ -18,8 +18,8 @@
  */
 int main()
 {
-    char opcion;
-    int n;
+    char opcion [2];
+    int n, vop;
 
     do
     {
@@ -33,28 +33,29 @@ int main()
             
             printf( "\n   Introduzca opción (1-4): ");
             fflush( stdin ); /* Se tiene que tener especial cuidado si utilizamos scanf para leer caracteres.Para resolver este problema, antes de leer un carácter con scanf, hay que vaciar (limpiar) el buffer del teclado. Para ello, se utiliza la función fflush.Puedes ampliar informacion en http://www.carlospes.com/curso_de_lenguaje_c/01_11_la_funcion_fflush.php */
-            scanf( "%c", &opcion );
-            
-        } while ( opcion < '1' || opcion > '4' );
+            scanf("%s", &opcion);
+            vop = atoi(opcion);
+        } while (vop < 1 || vop > 4);
 
-        switch ( opcion )
+        switch ( vop )
         {
-            case'1': printf( "\n   Calcular el IMC.");
+            case 1: printf( "\n   Calcular el IMC.");
                       /* Llamar al fichero IMC. */
                       printf( "\n   Hemos concluido el calculo del IMC. \n");
                       break;
 
-            case'2': printf( "\n   Convertir un número entero entre 1 y 3500 a número Romano.");
+            case 2: printf( "\n   Convertir un número entero entre 1 y 3500 a número Romano.");
                       /* LLamar al fichero para ejecutar la conversión. */
                       printf( "\n   Hemos concluido la conversión a número Romano. \n");
                       break;
 
-            case'3': printf( "\n   Introducir uno o varios sueldos, establecer cuál es el billete más grande a utilizar y que nos diga en cuantos billetes y monedas se descompone.\n");
+            case 3: printf( "\n   Introducir uno o varios sueldos, establecer cuál es el billete más grande a utilizar y que nos diga en cuantos billetes y monedas se descompone.\n");
                       /* Llamar al fichero principal que procesa los resultados del desgloce de billetes y monedas.*/
+                      SalarioXBilletes();  
                       printf( "\n   Hemos concluido el desgloce de Billetes y monedas. \n" );
          }
 
-    } while (opcion != '4') ;
+    } while (vop != 4) ;
 
     return 0;
 }
