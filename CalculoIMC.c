facb1e5b389d9b48088aa81e817a014a9ade47bc
Bernardo <Programacin@Sistemas35>
2016-09-06 11:02:19 +0200

    Eliminamos la funcion validasEntrada del codigo de este archivo, y se llama al archivo nuevo creado para dicha funcion que se utiliza tanto en el calculo del IMC como en SalarioXBilletes.

--- CalculoIMC.c
+++ CalculoIMC.c
@@ -63,27 +63,6 @@ do
 
 return (0);
 }
-        
-/* Función para validar la entrada sea un numero incluido decimal con un punto,
- *  no es valido para coma y no tiene en cuanta si se introducen dos veces un punto, por ej 5.6.7 daría valido.
- La función recibe la variable nom y devuelve la variable valido en uno si es un numero decimal de lo contrario devuelve valido en cero.*/
-int validasEntrada(char nom[]) 
-{ 
-int i = 0; 
-int valido = 0;// incializo la variable...es necesario.
-int numCarac = strlen(nom); 
-
-for( i = 0; i < numCarac; i++) 
-{ 
-    if((!isdigit(nom[i]))&& (nom[i]!=46)) 
-    {  
-        valido = 1;
-        break;
-    } 
-} 
-
-return valido; 
-} 
 /*Función para validar la entrada del sexo, Hombre corresponde a la H/h y Mujer M/m, 
  * si es correcto vsex = 1*/
 int validaSexo(char sex[]) 
